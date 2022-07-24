69be654c5a246f42e932c8fe2b0a2ea099a1169a
Fred GEFFRAY <in-touch-fg@quanticalsolutions.com>
2022-07-24 18:14:22 +0200

    first commit

--- migrations/Version20220724160855.php
+++ migrations/Version20220724160855.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220720063007 extends AbstractMigration
+final class Version20220724160855 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,13 +19,13 @@ final class Version20220720063007 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` ADD reference VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE `sessions` (`sess_id` VARBINARY(128) NOT NULL PRIMARY KEY,`sess_data` BLOB NOT NULL,`sess_lifetime` INTEGER UNSIGNED NOT NULL,`sess_time` INTEGER UNSIGNED NOT NULL,INDEX `sessions_sess_lifetime_idx` (`sess_lifetime`)) COLLATE utf8mb4_bin, ENGINE = InnoDB;');
+
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` DROP reference');
+        $this->addSql('DROP TABLE sessions');
+
     }
 }
